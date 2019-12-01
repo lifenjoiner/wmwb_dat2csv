536236b6147d4a9eb862018a0e22724565d24dd7
YX Hao <lifenjoiner@163.com>
2019-12-01 13:09:42 +0800

    accept version 2018 new dat formats

--- wmwb_dat2csv.c
+++ wmwb_dat2csv.c
@@ -1,6 +1,7 @@
 /* wmwb dat2csv
 
 alias
+wmwbh: wang ma wu bi hun
 wmwb: wang ma wu bi
 
 jm.dat: jian ma
@@ -44,10 +45,7 @@ https://zh.wikipedia.org/wiki/GB_18030
 #include <direct.h>
 
 /* **** */
-char *g_Ver = "0.2.1 #20180307";
-
-#define dict_item_qm_size 0x130
-#define dict_item_jm_size 0x08
+char *g_Ver = "0.2.2 #20191201";
 
 #if !defined(_STDINT) && !defined(_STDINT_H) && !defined(__int8_t_defined)
 typedef unsigned char uint8_t;
@@ -55,6 +53,7 @@ typedef unsigned short uint16_t;
 typedef unsigned long uint32_t;
 #endif
 
+/* < 2018 */
 /* 0x100, header
 */
 
@@ -95,65 +94,106 @@ typedef struct _DICT_ITEM_JM
     uint8_t word[4];
 } DICT_ITEM_JM, *PDICT_ITEM_JM;
 
+int dict_item_qm_size = 0x130;
+int dict_item_jm_size = 0x08;
+
+/* 2018 */
+typedef struct _DICT_ITEM_QM_XA
+{
+    uint32_t type;
+    uint32_t st;    // 1: simplified; 2: traditional; 3: 日、韩、香港地区汉字. output n-1
+    char code[4];
+    uint8_t word[296];
+} DICT_ITEM_QM_XA, *PDICT_ITEM_QM_XA;
+
+typedef struct _DICT_ITEM_JM_XA
+{
+    uint32_t idx;
+    uint8_t word[20];
+    uint8_t st;    // 0: simplified; 1: traditional
+} DICT_ITEM_JM_XA, *PDICT_ITEM_JM_XA;
+
+int dict_item_qm_size_xa = 0x134;
+int dict_item_jm_size_xa = 0x19;
+
+/* reform different version data */
+uint32_t ver_dat;
+
+typedef struct _DICT_ITEM
+{
+    uint8_t type;
+    char code[5];
+    uint8_t *word;
+    uint8_t st;
+} DICT_ITEM, *PDICT_ITEM;
+
 /*****/
 int convert_qm(FILE *fp_r, FILE *fp_w)
 {
-    DICT_ITEM_QM dict_item_qm;
-    int total_len, total_items, total_got, total_recognized, total_discard;
-    uint8_t CHWORD[5] = {0};
+    uint8_t *pdict_item_qm = NULL;
+    DICT_ITEM dict_item_qm = {0};
+    int total_len, counter, total_got, total_recognized, total_discard;
     //
+    if (ver_dat >= 0x0A) {
+        dict_item_qm_size = dict_item_qm_size_xa;
+    }
+    pdict_item_qm = malloc(dict_item_qm_size);
+    //
+    total_got = 0;
     total_len = _filelength(fileno(fp_r));
     total_len -= 0x100;
     if (total_len % dict_item_qm_size != 0) {
         fprintf(stderr, "Wrong dat file format!\n");
-        return 0;
+        goto cleanup;
     }
-    total_items = total_len / dict_item_qm_size;
-    fprintf(stdout, "items total: %d\n", total_items);
-    //
+    counter = total_len / dict_item_qm_size;
+    fprintf(stdout, "items total: %d\n", counter);
     //
     fseek(fp_r, 0x100, SEEK_SET);
     //
-    total_items = 0; // as index
-    total_got = 0;
+    counter = 0; // as index
     total_recognized = 0;
     total_discard = 0;
-    while (fread(&dict_item_qm, 1, dict_item_qm_size, fp_r) == dict_item_qm_size) {
+    while (fread(pdict_item_qm, 1, dict_item_qm_size, fp_r) == dict_item_qm_size) {
         int offset_word = 0;
-        total_items++;
+        counter++;
+        if (ver_dat < 0x0A) {
+            dict_item_qm.type = ((PDICT_ITEM_QM)pdict_item_qm)->type;
+            *(uint32_t*)dict_item_qm.code = *(uint32_t*)(((PDICT_ITEM_QM)pdict_item_qm)->code);
+            dict_item_qm.word = ((PDICT_ITEM_QM)pdict_item_qm)->word;
+        }
+        else {
+            dict_item_qm.type = ((PDICT_ITEM_QM_XA)pdict_item_qm)->type;
+            *(uint32_t*)dict_item_qm.code = *(uint32_t*)(((PDICT_ITEM_QM_XA)pdict_item_qm)->code);
+            dict_item_qm.word = ((PDICT_ITEM_QM_XA)pdict_item_qm)->word;
+            dict_item_qm.st = ((PDICT_ITEM_QM_XA)pdict_item_qm)->st - 1;
+        }
+        //
         switch (dict_item_qm.type) {
         case 0x01:
         case 0x10:
         case 0x02:
-            total_recognized++;
-            *(uint32_t*)CHWORD = *(uint32_t*)(dict_item_qm.word);
-            if (fprintf(fp_w, "%d\t", dict_item_qm.type) > 1
-                && fwrite(dict_item_qm.code, 1, 4, fp_w) == 4
-                && fputs("\t", fp_w) >= 0
-                && fputs(CHWORD, fp_w) >= 0
-                && fputs("\n", fp_w) >= 0) {
-                total_got++;
-            }
-            else {
-                fprintf(stderr, "unknown index: %d\n", total_items);
-            }
+            dict_item_qm.word[4] = 0; // drop following
             break;
         case 0x04:
             offset_word = 40;
         case 0x08:
-            total_recognized++;
-            if (fprintf(fp_w, "%d\t", dict_item_qm.type) > 1
-                && fwrite(dict_item_qm.code, 1, 4, fp_w) == 4
-                && fprintf(fp_w, "\t%s\n", dict_item_qm.word + offset_word) > 2) {
-                total_got++;
-            }
-            else {
-                fprintf(stderr, "unknown index: %d\n", total_items);
-            }
             break;
         default:
-            fprintf(stderr, "unknown type, total_items: %d\n", dict_item_qm.type, total_items);
-            break;
+            fprintf(stderr, "unknown type, counter: %d\n", dict_item_qm.type, counter);
+            continue;
+        }
+        //
+        total_recognized++;
+        dict_item_qm.word += offset_word;
+        if (fprintf(fp_w, "%d\t%s\t%s", dict_item_qm.type, dict_item_qm.code, dict_item_qm.word) > 7
+            && (ver_dat >= 0x0A ? fprintf(fp_w, "\t%d", dict_item_qm.st) > 1 : 1)
+            && fputs("\n", fp_w) >= 0
+        ) {
+            total_got++;
+        }
+        else {
+            fprintf(stderr, "unknown with index: %d\n", counter);
         }
     }
     fflush(fp_w);
@@ -162,16 +202,18 @@ int convert_qm(FILE *fp_r, FILE *fp_w)
     fprintf(stdout, "items got: %d\n", total_got);
     fprintf(stdout, "items discard: %d\n", total_discard);
     //
+cleanup:
+    free(pdict_item_qm);
     return total_got;
 }
 
-int lookup_jm_item(FILE *fp_r, int idx, PDICT_ITEM_JM pdict_item_jm)
+int lookup_jm_item(FILE *fp_r, int idx, uint8_t * pdict_item_jm)
 {   // l < b
     while (fread(pdict_item_jm, 1, dict_item_jm_size, fp_r) == dict_item_jm_size) {
-        if (pdict_item_jm->idx == idx) {
+        if (*(uint32_t*)pdict_item_jm == idx) {
             return 1;
         }
-        else if (pdict_item_jm->idx > idx) {
+        else if (*(uint32_t*)pdict_item_jm > idx) {
             fseek(fp_r, -dict_item_jm_size, SEEK_CUR);
             break;
         }
@@ -179,30 +221,52 @@ int lookup_jm_item(FILE *fp_r, int idx, PDICT_ITEM_JM pdict_item_jm)
     return 0;
 }
 
+int fwrite_jm_item(uint32_t code, uint8_t *pdict_item_jm, FILE *fp_w)
+{
+    if (ver_dat < 0x0A) {
+        uint8_t CHWORD[5] = {0}; // strip next index
+        *(uint32_t*)CHWORD = *(uint32_t*)(((PDICT_ITEM_JM)pdict_item_jm)->word);
+        fprintf(fp_w, "1\t%s\t%s\n", &code, CHWORD);
+    }
+    else {
+        fprintf(fp_w, "1\t%s\t%s\t%d\n", &code,
+            ((PDICT_ITEM_JM_XA)pdict_item_jm)->word,
+            ((PDICT_ITEM_JM_XA)pdict_item_jm)->st
+        );
+    }
+}
+
 int convert_jm(FILE *fp_r, FILE *fp_w)
 {
-    DICT_ITEM_JM dict_item_jm;
+    uint8_t *pdict_item_jm;
     int total_len, total_items, total_got;
     int k1, k2, k3, idx;
-    uint8_t CHWORD[5] = {0};
+    int sets;
     //
+    if (ver_dat >= 0x0A) {
+        dict_item_jm_size = dict_item_jm_size_xa;
+    }
+    pdict_item_jm = malloc(dict_item_jm_size);
+    //
+    total_got = 0;
     total_len = _filelength(fileno(fp_r));
     total_len -= 0x100;
     if (total_len % dict_item_jm_size != 0) {
         fprintf(stderr, "Wrong dat file format!\n");
-        return 0;
+        goto cleanup;
     }
     total_items = total_len / dict_item_jm_size;
     fprintf(stdout, "items total: %d\n", total_items);
     //
-    total_got = 0;
-    // 1, 25
     fseek(fp_r, 0x100, SEEK_SET);
+
+    sets = 0;
+dump_jm:
+    // 1, 25
     for (k1 = 'a'; k1 < 'z'; k1++) {
         idx = k1 - 97;
-        while (lookup_jm_item(fp_r, idx, &dict_item_jm) > 0) { // >=1
-            *(uint32_t*)CHWORD = *(uint32_t*)(dict_item_jm.word);
-            fprintf(fp_w, "1\t%c\t%s\n", k1, CHWORD);
+        if (lookup_jm_item(fp_r, idx, pdict_item_jm) > 0) { // >=1
+            fwrite_jm_item(k1, pdict_item_jm, fp_w);
             total_got++;
         }
     }
@@ -210,9 +274,8 @@ int convert_jm(FILE *fp_r, FILE *fp_w)
     for (k1 = 'a'; k1 < 'z'; k1++) {
         for (k2 = 'a'; k2 < 'z'; k2++) {
             idx = 30 * (k1 - 96) + k2 - 96;
-            while (lookup_jm_item(fp_r, idx, &dict_item_jm) > 0) {
-                *(uint32_t*)CHWORD = *(uint32_t*)(dict_item_jm.word);
-                fprintf(fp_w, "1\t%c%c\t%s\n", k1, k2, CHWORD);
+            while (lookup_jm_item(fp_r, idx, pdict_item_jm) > 0) {
+                fwrite_jm_item(k1 | k2<<8, pdict_item_jm, fp_w);
                 total_got++;
             }
         }
@@ -222,18 +285,27 @@ int convert_jm(FILE *fp_r, FILE *fp_w)
         for (k2 = 'a'; k2 < 'z'; k2++) {
             for (k3 = 'a'; k3 < 'z'; k3++) {
                 idx = 30 * (30 * (k1 - 96) + k2 - 96) + k3 - 96;
-                while (lookup_jm_item(fp_r, idx, &dict_item_jm) > 0) {
-                    *(uint32_t*)CHWORD = *(uint32_t*)(dict_item_jm.word);
-                    fprintf(fp_w, "1\t%c%c%c\t%s\n", k1, k2, k3, CHWORD);
+                while (lookup_jm_item(fp_r, idx, pdict_item_jm) > 0) {
+                    if (ver_dat >= 0x0A && ((PDICT_ITEM_JM_XA)pdict_item_jm)->st != sets) continue;
+                    fwrite_jm_item(k1 | k2<<8 | k3<<16, pdict_item_jm, fp_w);
                     total_got++;
                 }
             }
         }
     }
+    sets++;
+    // traditional
+    if (ver_dat >= 0x0A && sets < 2) {
+        fseek(fp_r, 0x100 + total_got * dict_item_jm_size, SEEK_SET);
+        goto dump_jm;
+    }
+
     fflush(fp_w);
     //
     fprintf(stdout, "items got: %d\n", total_got);
     //
+cleanup:
+    free(pdict_item_jm);
     return total_got;
 }
 
@@ -247,8 +319,9 @@ int main(int argc, char **argv)
     if (argc != 4) {
         fprintf(stderr, "wmwb dat file extractor (GB 18030-2000) v%s @YX Hao\n", g_Ver);
         fprintf(stderr, "Usage: %s <q|j> <input-dat-file> <ouput-csv-file>\n", argv[0]);
-        fprintf(stderr, "   q   qm dat file\n", argv[0]);
-        fprintf(stderr, "   j   jm dat file\n", argv[0]);
+        fprintf(stderr, "   q   qm dat file\n");
+        fprintf(stderr, "   j   jm dat file\n");
+        fprintf(stderr, "sets: 0 = simplified; 1 = traditional; 2 = jp, kr, hk, vi, etc.\n");
         return 1;
     }
     //
@@ -261,6 +334,12 @@ int main(int argc, char **argv)
     }
     fprintf(stdout, "%s >>\n", filename_dat);
     //
+    fseek(fp_r, 0x80, SEEK_SET);
+    if (fread(&ver_dat, 1, 4, fp_r) != 4) {
+        fprintf(stderr, "too small file size!\n");
+        goto error;
+    }
+    //
     fp_w = fopen(filename_csv, "wb");
     if (!fp_w) {
         fprintf(stderr, "Can't open file!\n");
@@ -268,7 +347,12 @@ int main(int argc, char **argv)
     }
     fprintf(stdout, "%s <<\n", filename_csv);
     //
-    fprintf(fp_w, "%type\tkeys\twords\n");
+    if (ver_dat < 0x0A) {
+        fprintf(fp_w, "%type\tkeys\twords\n");
+    }
+    else {
+        fprintf(fp_w, "%type\tkeys\twords\tsets\n");
+    }
     switch (argv[1][0]) {
     case 'q':
         ret = convert_qm(fp_r, fp_w) > 0;
